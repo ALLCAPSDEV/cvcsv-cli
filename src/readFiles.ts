0e34525878ae7ab0f6acc44ad899b7e3a7dfd68e
Matt Riley <matt.riley@metrisk.com>
2019-09-05 16:13:03 +0100

    Chore: Refactored the dir existing error

--- src/readFiles.ts
+++ src/readFiles.ts
@@ -1,17 +1,7 @@
 import globby from 'globby';
-import fs from 'fs';
-import { Logger } from './utils/logger';
 
 export const readFiles = async (dirPath?: string) => {
   const defaultPath = dirPath ? dirPath : './';
-  if (process.env.NODE_ENV !== 'test') {
-    fs.access(defaultPath, err => {
-      if (err && err.code === 'ENOENT') {
-        const logger = new Logger;
-        logger.error("Directory doesn't exist");
-      }
-    })
-  }
   const paths = await globby(defaultPath, {
     expandDirectories: {
       files: ['*'],
