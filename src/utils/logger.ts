b5d614e6af18f1ebcbe624d82dc71b940a20a5b0
Matt Riley <matt.riley@metrisk.com>
2019-09-05 15:45:00 +0100

    Fix: Errors if dir doesn't exist

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,8 @@
 import {bold, blue} from 'kleur';
 import {ConsoleMessage} from '../interfaces/Console';
 import figlet from 'figlet';
-import { exists } from 'fs';
 
 export class Logger {
-
   public showTitleAndBanner(): void {
     console.log(bold().blue(figlet.textSync(ConsoleMessage.TITLE, { font: "Graffiti"})));
     console.log(blue(ConsoleMessage.BANNER));
