5cbc33aa333a2ad1125a16340e4067cb7395332b
Matt Riley <matt.riley@metrisk.com>
2019-09-05 16:13:03 +0100

    Chore: Refactored the dir existing error

--- src/questions/directoryQuestion.ts
+++ src/questions/directoryQuestion.ts
@@ -1,11 +1,19 @@
-import inquirer from 'inquirer';
+import inquirer, {InputQuestion} from 'inquirer';
 import {blue} from 'kleur';
+import fs from 'fs';
 
-const directoryQuestion: inquirer.Question = {
+const directoryQuestion: inquirer.Question<InputQuestion> = {
   name: 'directory',
   type: 'input',
   message: `${blue("Enter the root directory: ")}`,
-  default: './'
+  default: './',
+  validate: value => {
+    if(fs.existsSync(value)) {
+      return true;
+    }
+
+    return "😱 Sorry! The directory doesn't exist, please try again"
+  }
 }  
 
 export default directoryQuestion;
