7bde7e3854c903bea3cfe56f5ada2de3d5bb11ad
Matt Riley <matt.riley@metrisk.com>
2020-02-21 12:15:34 +0000

    test(configfile): added some tests

---
+++ src/interfaces/ReadFileResponse.ts
@@ -0,0 +1,9 @@
+export interface ReadFileResponse {
+  bucketName?: string;
+  csvFileLocation?: string;
+  csvFilename?: string;
+  productCategory?: string;
+  productSet?: string;
+  rootDirectory?: string;
+  vertices?: string;
+}
