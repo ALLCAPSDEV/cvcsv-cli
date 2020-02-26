3f61623a46f542ff00c733068324f58acf51cf98
Matt Riley <matt.riley@metrisk.com>
2020-02-26 12:52:01 +0000

    test(more tests): added more tests so they are actually valuable

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,40 +1,35 @@
-import { bold, blue, green } from "kleur";
+import { bold, blue } from "kleur";
 import { ConsoleMessage } from "../interfaces/Console";
 import figlet from "figlet";
 
 export class Logger {
-  public showTitleAndBanner(): void {
-    console.log(
-      bold().blue(figlet.textSync(ConsoleMessage.TITLE, { font: "Graffiti" }))
+  public static showTitleAndBanner(): void {
+    this.log(
+      `${bold().blue(
+        figlet.textSync(ConsoleMessage.TITLE, { font: "Graffiti" })
+      )}\n\n${blue(ConsoleMessage.BANNER)}\n\n`
     );
-    console.log(blue(ConsoleMessage.BANNER));
-    console.log("\n");
   }
 
-  public success(files: number): void {
+  public static success(files: number): void {
     const plural = files > 1 ? "files" : "file";
-    console.log("\n");
-    console.log(
-      bold()
+    this.log(
+      `\n\n${bold()
         .bgWhite()
-        .blue(`Added ${files} ${plural} to your CSV file.`)
-    );
-    console.log("\n");
-    console.log(
-      bold().blue(
+        .blue(`Added ${files} ${plural} to your CSV file.`)}\n${bold().blue(
         figlet.textSync(ConsoleMessage.DONE, {
           font: "Graffiti"
         })
-      )
+      )}`
     );
   }
 
-  public error(msg: string): void {
-    console.log(bold().red(`${ConsoleMessage.ERROR}${msg}`));
+  public static error(msg: string): void {
+    this.log(bold().red(`${ConsoleMessage.ERROR}${msg}`), true);
     process.exit(1);
   }
 
-  public msg(msg: string): void {
-    console.log(green(`${msg}`));
+  private static log(message: string, error?: boolean): void {
+    console[error ? "error" : "log"](message);
   }
 }
