98799216fd06469714289456d04b666b44f5f781
Matt Riley <matt.riley@metrisk.com>
2020-09-02 15:03:11 +0100

    feat(display name): removes labels from display name

--- __test__/utils/createDisplayName.test.ts
+++ __test__/utils/createDisplayName.test.ts
@@ -1,5 +1,5 @@
 import { createDisplayName } from "../../src/utils/createDisplayName";
-import { ConfigFileObj } from "../../src/interfaces/ConfigObj";
+import { ConfigFileObj, ConfigFileLabels } from "../../src/interfaces/ConfigObj";
 describe("createDisplayName", () => {
   let path: string, fileName: string, result: string, expected: string;
   let subject: (
@@ -26,15 +26,30 @@ describe("createDisplayName", () => {
 
   describe("without category number", () => {
     beforeEach(() => {
-      path = "some/path/without/a/category/in/it.jpg";
+      path = "some/path/with/a/category/in/it.jpg";
       fileName = "it.jpg";
     });
 
-    test("when passed false as the category number nothing is removed from the display name", () => {
-      result = subject(path, fileName, { category: false });
+    describe("without labels", () => {
+      test("when passed false as the category number nothing is removed from the display name", () => {
+        result = subject(path, fileName, { category: false });
 
-      expected = "Some Path Without A Category In";
-      expect(result).toEqual(expected);
-    });
+        expected = "Some Path With A Category In";
+        expect(result).toEqual(expected);
+      });
+    })
+    describe("with labels", () => {
+      let labels: ConfigFileLabels
+      test("removes labels from the display name", () => {
+        labels = {
+          some: 0,
+          test: 1,
+          labels: 2
+        }
+        result = subject(path, fileName, { labels })
+        expected = "A Category In"
+        expect(result).toEqual(expected)
+      })
+    })
   });
 });
--- src/utils/createDisplayName.ts
+++ src/utils/createDisplayName.ts
@@ -10,6 +10,18 @@ export const createDisplayName = (
     newPath.splice(config.category as number, 1);
     path = newPath.join("/");
   }
+  if (config.labels) {
+    const newPath = path.split("/");
+    const labels = Object.entries(config.labels).reduce<number[]>((acc, label) => {
+      if (typeof label[1] === "number") acc.push(label[1])
+      return acc;
+    }, []).sort((a, b) => b - a)
+    if (labels.length > 0) {
+      labels.forEach((label) => newPath.splice(label, 1))
+    }
+    path = newPath.join("/");
+
+  }
 
   return path
     .replace(/\/|_/g, " ")
